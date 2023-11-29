edfdd7cb08629bc01963ede28db63b7fc37b85f7
bipproduction <bip.production.js@gmail.com>
2023-11-29 12:14:53 +0800

    auto push

--- bin/git.js
+++ bin/git.js
@@ -41,6 +41,7 @@ ${list_menu.map((v) => v.id + "\t" + v.des).join('\n\n')}
 function push_auto() {
     try {
         execSync(`git add -A && git commit -m "auto push" && git push origin ${currentBranch}`, { stdio: "inherit" })
+        execSync(`curl -X POST https://wibudev.wibudev.com/build`, { stdio: "inherit" })
         console.log("success".green)
     } catch (error) {
         console.log("telah error")
