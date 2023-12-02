1844fd993919dd2f13734b15caa8a09e2bc70de0
bipproduction <bip.production.js@gmail.com>
2023-12-02 19:50:43 +0800

    auto push

--- bin/version.js
+++ bin/version.js
@@ -4,8 +4,8 @@
  * @returns string
  */
 function version(ver) {
+    if (!ver) throw new Error("ver tidak boleh kosong")
     const v = "1.0.0".split('.').map(Number)
     v[2]++;
     return v.join(".")
-    
 }
--- bin_tmp/version.js
+++ bin_tmp/version.js
@@ -5,8 +5,8 @@ const root = require('child_process').execSync('npm root -g').toString().trim();
  * @returns string
  */
 function version(ver) {
+    if (!ver) throw new Error("ver tidak boleh kosong")
     const v = "1.0.0".split('.').map(Number)
     v[2]++;
     return v.join(".")
-    
 }
