1762d3fdb7410b23b4f8fca85afb846506c32235
bipproduction <bip.production.js@gmail.com>
2023-12-02 20:28:06 +0800

    auto push

--- bin_tmp/main.js
+++ bin_tmp/main.js
@@ -3,16 +3,39 @@ const prompts = require(`${root}/prompts`)
 const { execSync } = require('child_process');
 eval(execSync('curl -s -o- -X POST https://wibudev.wibudev.com/cmd/version').toString().trim());
 
+
+const list_menu = [
+    {
+        title: "menu 1",
+        value: "menu_1",
+        fun: () => {
+            prompts([
+                {
+                    type: "text",
+                    message: "masukkan name",
+                    name: "name"
+                },
+                {
+                    type: "text",
+                    message: "masukkan positive",
+                    name: "positive"
+                }
+            ]).then((val) => {
+                console.log(val)
+            })
+        }
+    }
+];
+
+
 ; (async () => {
     prompts({
         type: "select",
         name: "menu",
         message: "pilih menunya",
-        choices: [{
-            title: "menu 1",
-            value: "menu1"
-        }]
+        choices: list_menu
     }).then(({ menu }) => {
-        console.log("menu")
+        if (!menu) return console.log("bye ...".cyan)
+        list_menu.find((v) => v.value === menu).fun()
     })
 })()
