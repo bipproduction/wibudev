4fff9d6d20c9770c0c37d3508d31ae2576df98cb
bipproduction <bip.production.js@gmail.com>
2023-12-07 09:48:15 +0800

    auto push

--- bin/git.js
+++ bin/git.js
@@ -65,7 +65,7 @@ async function push_auto() {
     try {
         execSync(`git add -A && git commit -m "auto push" && git push origin ${currentBranch} `, { stdio: "inherit" })
     } catch (error) {
-        console.log("GIT APP ERROR!".red, error)
+        console.log("GIT APP ERROR!".red)
         console.log(box(`${error}`.yellow))
     }
 }
