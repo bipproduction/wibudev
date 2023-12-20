1d8fb5255f8f5e75deda482c932d853c9ac62faf
bipproduction <bip.production.js@gmail.com>
2023-12-20 12:18:54 +0800

    auto push

--- src/fun/bin/git.js
+++ src/fun/bin/git.js
@@ -2,7 +2,9 @@ const _ = require('lodash')
 const yargs = require('yargs')
 const { execSync } = require('child_process')
 const { box } = require('teeti')
+const loading = (require('loading-cli'))("loading ...")
 module.exports = async function (param) {
+    loading.start()
     const currentBranch = execSync('git branch --show-current').toString().trim();
     const args = yargs
         .scriptName("git")
@@ -12,15 +14,18 @@ module.exports = async function (param) {
         .parse()
 
     if (args._[1] === "push") {
+        loading.stop()
         execSync(`git add -A && git commit -m "auto push" && git push origin ${currentBranch}`)
         return console.log(box("SUCCESS").green)
     }
 
     if (args._[1] === "pull") {
 
+        loading.stop()
         return
     }
 
+    loading.stop()
     yargs.showHelp()
 
 
