cbb9aa6670213a641cd11a7cdb863f5284783891
bipproduction <bip.production.js@gmail.com>
2023-12-21 14:56:25 +0800

    auto push

--- src/fun/bin/_dev.js
+++ src/fun/bin/_dev.js
@@ -1,19 +1,36 @@
 const columnify = require("columnify")
 const yargs = require('yargs')
 const _ = require('lodash')
+const { fetch } = require('cross-fetch')
+const { box } = require('teeti')
+require('colors')
 
 module.exports = async function (param) {
     const apps = param.apps.map((v) => ({ ..._.omit(v, ['script']) }))
 
     const arg = yargs
-        .command("list-app")
+        .command("list-app", "ini list app")
+        .command('set-dev', "set developer")
+        .option('host-name', {
+            alias: "h",
+            string: true
+        })
         .version("1.0.0")
-        .parse()
+        .argv
 
     if (arg._[1] === "list-app") {
         console.log(columnify(apps).gray)
         return
     }
 
-    yargs.showHelp()
+    if (arg._[1] === "set-dev") {
+        if (!arg.hostName) return console.log(box("require host-name").yellow)
+        const res = await fetch(`${param.url_pro}/config?host_name=${arg.hostName}`)
+        const data = await res.json()
+        console.log(box( data.host_name).green)
+        return
+    }
+
+    // yargs.showHelp()
 }
+
