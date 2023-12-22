f9d5c9e8b37ed265eb4039a7c4963cd83991fa81
bipproduction <bip.production.js@gmail.com>
2023-12-22 14:12:30 +0800

    auto push

--- src/fun/util/not-found.js
+++ src/fun/util/not-found.js
@@ -2,11 +2,14 @@ const yargs = require('yargs')
 const { hideBin } = require('yargs/helpers')
 require('colors')
 
+const ascify = require('asciify')
+ascify("WIBUDEV", {font: "Standard"}, (e, val) => console.log(val.cyan))
+
 module.exports = async function (param) {
     const com = param.bin.map((v) => [v])
     yargs(hideBin(process.argv))
         .command([...com])
         .version("1.0.0")
-        .epilog("Beberapa Menu Masih Dalam Pengembangran".cyan)
+        .epilog("Beberapa Menu Masih Dalam Pengembangran".gray)
         .showHelp()
 }
