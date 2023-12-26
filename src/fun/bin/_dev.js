30577b069299078d95056ddc5e96680f72224a87
bipproduction <bip.production.js@gmail.com>
2023-12-26 09:12:29 +0800

    auto push

--- src/fun/bin/_dev.js
+++ src/fun/bin/_dev.js
@@ -35,15 +35,7 @@ module.exports = async function (param) {
     }
 
     if (arg._[1] === "list-server") {
-        const ls = execSync('ls /etc/nginx/sites-enabled').toString().trim()
-        const lines = ls.trim().split('\n');
-        const serverJson = _.chain(lines)
-            .map(line => line.split(/\s+/))
-            .map(([key, ...values]) => ({ [key]: values }))
-            .reduce(_.merge)
-            .value();
-
-        console.log(columnify(serverJson))
+       
     }
 
     yargs.showHelp()
---
+++ src/svr/list-server.js
@@ -0,0 +1,15 @@
+const { execSync } = require('child_process');
+const columnify = require('columnify');
+const _ = require('lodash');
+
+; (async () => {
+    const ls = execSync('ls /etc/nginx/sites-enabled').toString().trim()
+    const lines = ls.trim().split('\n');
+    const serverJson = _.chain(lines)
+        .map(line => line.split(/\s+/))
+        .map(([key, ...values]) => ({ [key]: values }))
+        .reduce(_.merge)
+        .value();
+
+    console.log(columnify(serverJson))
+})()
