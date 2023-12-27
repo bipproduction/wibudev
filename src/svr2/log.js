bdab676ea85677f047e3987b9ed8b7f8386a7bd2
bipproduction <bip.production.js@gmail.com>
2023-12-27 14:58:02 +0800

    auto push

--- src/svr2/log.js
+++ src/svr2/log.js
@@ -1,6 +1,9 @@
 const { exec } = require('child_process')
 module.exports = function (param) {
     const child = exec('pm2 log wibudev')
+    setTimeout(() => {
+        child.kill()
+    }, 5000)
     return {
         stdout: child.stdout,
         stderr: child.stderr,
