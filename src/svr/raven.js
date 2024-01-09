63d1d79cf67aee92775f8b8c3686ec87fa81d507
bipproduction <bip.production.js@gmail.com>
2024-01-09 12:02:45 +0800

    auto push

--- src/svr/raven.js
+++ src/svr/raven.js
@@ -7,7 +7,7 @@ const yargs = require("yargs")
 const argv = yargs.parse(arg)
 
 
-const dir = argv.T ? "test-raven-stone2" : "raven-stone2"
+const dir = argv.T  === 'true' ? "test-raven-stone2" : "raven-stone2"
 console.log(arg, dir, argv)
 const child = spawn('/bin/bash', ['-c', `cd ${path.join(__dirname, `./../../../${dir}`)} && cat ${path.join(__dirname, "./_raven.js")} | node - ${arg.join(" ")}`])
 child.stdout.on("data", data => console.log(data.toString()))
