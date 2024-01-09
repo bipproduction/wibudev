fac15a98221d0181c4caba6877b7b39651949495
bipproduction <bip.production.js@gmail.com>
2024-01-09 11:59:09 +0800

    auto push

--- src/svr/raven.js
+++ src/svr/raven.js
@@ -6,6 +6,7 @@ console.log(arg)
 const yargs = require("yargs")
 const argv = yargs.parse(arg)
 
+
 const dir = argv.T ? "test-raven-stone2" : "raven-stone2"
 const child = spawn('/bin/bash', ['-c', `cd ${path.join(__dirname, `./../../../${dir}`)} && cat ${path.join(__dirname, "./_raven.js")} | node - ${arg.join(" ")}`])
 child.stdout.on("data", data => console.log(data.toString()))
